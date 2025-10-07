c9625aebf58709b61230973a896f50a6a2931c75
KShivendu <kshivendu1@gmail.com>
2025-10-07 21:16:42 +0530

    Allow replicating points between shards

--- lib/collection/src/shards/transfer/driver.rs
+++ lib/collection/src/shards/transfer/driver.rs
@@ -59,13 +59,26 @@ pub async fn transfer_shard(
 
     match transfer_config.method.unwrap_or_default() {
         // Transfer shard record in batches
-        ShardTransferMethod::StreamRecords | ShardTransferMethod::FilteredStreamRecords => {
+        ShardTransferMethod::StreamRecords => {
             transfer_stream_records(
                 shard_holder.clone(),
                 progress,
                 local_shard_id,
                 remote_shard,
                 &collection_id,
+                None,
+            )
+            .await?;
+        }
+
+        ShardTransferMethod::FilteredStreamRecords => {
+            transfer_stream_records(
+                shard_holder.clone(),
+                progress,
+                local_shard_id,
+                remote_shard,
+                &collection_id,
+                transfer_config.filter,
             )
             .await?;
         }
