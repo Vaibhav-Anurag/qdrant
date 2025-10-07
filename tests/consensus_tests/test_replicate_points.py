dc6fda7de2f9e07f245f9155211dee3a4beb3504
KShivendu <kshivendu1@gmail.com>
2025-10-07 23:36:07 +0530

    Respect payload and hashring filters while replicating point

--- tests/consensus_tests/test_replicate_points.py
+++ tests/consensus_tests/test_replicate_points.py
@@ -1,15 +1,11 @@
-import multiprocessing
 import pathlib
-import random
-from time import sleep
 
 from .fixtures import upsert_random_points, create_collection
 from .utils import *
-
 from .test_custom_sharding import create_shard
 
 N_PEERS = 1
-N_SHARDS = 1
+N_SHARDS = 2
 N_REPLICA = 1
 COLLECTION_NAME = "test_collection"
 
@@ -56,41 +52,53 @@ def test_replicate_points_stream_transfer(tmp_path: pathlib.Path):
     from_peer_id = transfer_collection_cluster_info["peer_id"]
     to_peer_id = receiver_collection_cluster_info["peer_id"]
 
-    from_shard_id = transfer_collection_cluster_info["local_shards"][0]["shard_id"]
-    to_shard_id = receiver_collection_cluster_info["local_shards"][-1]["shard_id"]
+    default_key_shard_ids = []
+    tenant_key_shard_ids = []
+
+    for shard in transfer_collection_cluster_info["local_shards"]:
+        if shard["shard_key"] == "default":
+            default_key_shard_ids.append(shard["shard_id"])
+        elif shard["shard_key"] == "tenant":
+            tenant_key_shard_ids.append(shard["shard_id"])
 
     # Transfer shard from one node to another
+    # filter = {"should": {"key": "city", "match": {"value": "London"}}}
     filter = {}
 
-    # Move points from from_shard_id to to_shard_id
-    r = requests.post(
-        f"{peer_api_uris[0]}/collections/{COLLECTION_NAME}/cluster",
-        json={
-            "replicate_points": {
-                "filter": filter,
-                "from_shard_id": from_shard_id,
-                "to_shard_id": to_shard_id,
-                "from_peer_id": from_peer_id,
-                "to_peer_id": to_peer_id,
-            }
-        },
-    )
-    assert_http_ok(r)
+    remaining_transfers = len(default_key_shard_ids) * len(tenant_key_shard_ids)
 
-    # Wait for end of shard transfer
-    wait_for_collection_shard_transfers_count(peer_api_uris[0], COLLECTION_NAME, 0)
+    for from_shard_id in default_key_shard_ids:
+        for to_shard_id in tenant_key_shard_ids:
+            # Move points from from_shard_id to to_shard_id
+            r = requests.post(
+                f"{peer_api_uris[0]}/collections/{COLLECTION_NAME}/cluster",
+                json={
+                    "replicate_points": {
+                        "filter": filter,
+                        "from_shard_id": from_shard_id,
+                        "to_shard_id": to_shard_id,
+                        "from_peer_id": from_peer_id,
+                        "to_peer_id": to_peer_id,
+                    }
+                },
+            )
+            assert_http_ok(r)
 
-    receiver_collection_cluster_info = get_collection_cluster_info(
-        peer_api_uris[-1], COLLECTION_NAME
-    )
-    number_local_shards = len(receiver_collection_cluster_info["local_shards"])
-    assert number_local_shards == 2
+            # Wait for end of shard transfer
+            wait_for_collection_shard_transfers_count(peer_api_uris[0], COLLECTION_NAME, 0)
+            remaining_transfers -= 1
 
-    # Point counts must be consistent across shard keys for given filter
-    for shard_key in ["default", "tenant"]:
-        r = requests.post(
-            f"{peer_api_uris[0]}/collections/{COLLECTION_NAME}/points/count",
-            json={"exact": True, "filter": filter, "shard_key": shard_key},
-        )
-        assert_http_ok(r)
-        assert r.json()["result"]["count"] == 100
+            # Point counts must be consistent across shard keys for given filter
+            counts = []
+            for shard_key in ["default", "tenant"]:
+                r = requests.post(
+                    f"{peer_api_uris[0]}/collections/{COLLECTION_NAME}/points/count",
+                    json={"exact": True, "filter": filter, "shard_key": shard_key},
+                )
+                assert_http_ok(r)
+                counts.append(r.json()["result"]["count"])
+
+            if remaining_transfers == 0:
+                assert counts[0] == counts[1]
+            else:
+                assert counts[0] > counts[1]
