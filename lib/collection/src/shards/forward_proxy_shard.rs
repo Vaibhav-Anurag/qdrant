68daa3fedebbde2c9a2405203dbf9f2b158320f7
KShivendu <kshivendu1@gmail.com>
2025-10-08 14:12:50 +0530

    Merge points instead of replacing for filtered stream records transfer

--- lib/collection/src/shards/forward_proxy_shard.rs
+++ lib/collection/src/shards/forward_proxy_shard.rs
@@ -52,6 +52,8 @@ pub struct ForwardProxyShard {
     /// Lock required to protect transfer-in-progress updates.
     /// It should block data updating operations while the batch is being transferred.
     update_lock: Mutex<()>,
+    // ToDo: Transfer points only if they match the filter
+    filter: Option<Box<Filter>>,
 }
 
 impl ForwardProxyShard {
@@ -84,6 +86,7 @@ impl ForwardProxyShard {
             wrapped_shard,
             remote_shard,
             resharding_hash_ring,
+            filter: filter.map(Box::new),
             update_lock: Mutex::new(()),
         }
     }
@@ -139,7 +142,7 @@ impl ForwardProxyShard {
                     offset,
                     batch_size,
                     hashring_filter,
-                    self.filter.as_ref(),
+                    self.filter.as_deref(),
                     runtime_handle,
                 )
                 .await?
